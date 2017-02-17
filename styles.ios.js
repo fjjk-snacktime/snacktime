15f6bc682879990bb4b3d50771c0a6e935bd37e4
xfredlix <fred.m.li.93@gmail.com>
2017-02-17 11:50:17 -0800

    took out the stylesheet from the components to its own page

---
+++ styles.ios.js
@@ -0,0 +1,35 @@
+import {
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  takePicture: {
+    marginTop: 450,
+    height: 50,
+    width: 50,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+});

cd9e9c0ae71b49008c4ffb34467432fdf3a6f799
xfredlix <fred.m.li.93@gmail.com>
2017-02-17 17:09:44 -0800

    formatted the layout of navbar camera button and main view

--- styles.ios.js
+++ styles.ios.js
@@ -10,8 +10,12 @@ export default styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  app: {
+    flex: 6,
+    width: Dimensions.get('window').width,
+  },
   welcome: {
-    fontSize: 20,
+    fontSize: 60,
     textAlign: 'center',
     margin: 10,
   },
@@ -20,10 +24,12 @@ export default styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  buttonView:{
+    flex: 1,
+  },
   takePicture: {
-    marginTop: 450,
-    height: 50,
-    width: 50,
+    height: 100,
+    width: 100,
   },
   preview: {
     flex: 1,
@@ -32,4 +38,51 @@ export default styles = StyleSheet.create({
     height: Dimensions.get('window').height,
     width: Dimensions.get('window').width
   },
+  navigation: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    flex: 1,
+    width: Dimensions.get('window').width,
+    margin: 0, 
+    backgroundColor: 'deepskyblue',    
+    justifyContent: 'center',
+  },
+
+  glyphicon: {
+    flexDirection: 'row',
+    height: 60,
+    width: 60,
+    padding: 5,
+    marginRight: 60,
+  },
+
+  navigationahover: {
+    backgroundColor: 'deepskyblue',
+  },
+
+// @media all and (max-width: 800px) {
+//   .navigation {
+//     justify-content: space-around;
+//   }
+// }
+
+// @media all and (max-width: 600px) {
+//   .navigation {
+//     -webkit-flex-flow: column wrap;
+//     flex-flow: column wrap;
+//     padding: 0;
+//   }
+  
+//   .navigation a { 
+//     text-align: center; 
+//     padding: 10px;
+//     border-top: 1px solid rgba(255,255,255,0.3); 
+//     border-bottom: 1px solid rgba(0,0,0,0.1); 
+//   }
+
+  
+//   .navigation li:last-of-type a {
+//     border-bottom: none;
+//   }
+
 });

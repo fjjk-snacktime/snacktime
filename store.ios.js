e1720581f723d14c4677f0ad598f04fc6a879490
xfredlix <fred.m.li.93@gmail.com>
2017-02-18 11:31:35 -0800

    added a store to create our store

---
+++ store.ios.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, createStore, compose} from "redux"
+
+import logger from "redux-logger"
+import thunk from "redux-thunk"
+import promise from "redux-promise-middleware"
+
+import reducer from "./reducers"
+
+const middleware = applyMiddleware(promise(), thunk, logger())
+
+export default createStore(
+  reducer, 
+  compose(
+    middleware,
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);

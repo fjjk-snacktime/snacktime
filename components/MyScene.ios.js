b9491fda67ac8a29f04bee65058e355f345afebf
Kevin Wong <kkwokwai22@gmail.com>
2017-02-21 16:58:24 -0800

    working on feature

---
+++ components/MyScene.ios.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, Navigator } from 'react-native';
+
+export default class MyScene extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hi! My name is kevin</Text>
+      </View>
+    )
+  }
+}

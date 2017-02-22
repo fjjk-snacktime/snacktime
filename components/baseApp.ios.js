c248445833f8ae0ce560d556ae55fae7b48c13e5
xfredlix <fred.m.li.93@gmail.com>
2017-02-21 16:33:34 -0800

    added a navigator in the props

--- components/baseApp.ios.js
+++ components/baseApp.ios.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import { Provider } from "react-redux";
 import App from './App.ios.js';
+import camera from './Camera.ios.js';
 import store from '../store.ios.js';
+import Welcome from './Welcome.ios.js';
+import { NavigatorIOS } from 'react-native';
+import styles from '../styles.ios.js';
 
-export default class snacktime extends Component {
+export default class BaseApp extends Component {
   render() {
     return (
       <Provider store={store}>
-        <App />
+        <App navigator={this.props.navigator}/>
       </Provider>
     );
   }

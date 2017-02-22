964ecae543a716a0cb8308bb185a8527380fd9e2
xfredlix <fred.m.li.93@gmail.com>
2017-02-21 16:34:08 -0800

    changed the base app from baseapp to navigator

--- index.ios.js
+++ index.ios.js
@@ -6,6 +6,6 @@
 
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import snacktime from './components/baseApp.ios.js';
+import snacktime from './components/Navigator.ios.js';
 
 AppRegistry.registerComponent('snacktime', () => snacktime);

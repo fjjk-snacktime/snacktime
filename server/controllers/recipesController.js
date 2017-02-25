7510899ad31f228d203cdf86c55faf89fd3ecf1a
xfredlix <fred.m.li.93@gmail.com>
2017-02-24 16:58:55 -0800

    updated server config to have helper function

--- server/controllers/recipesController.js
+++ server/controllers/recipesController.js
@@ -2,16 +2,13 @@ const axios = require('axios');
 
 const recipes = {
   getRecipeList: (req, res, next) => {
-    console.log('request body data', req.body.data);
     const options = {
-      headers: { "X-Mashape-Key": "f9B4tJmyAkmshyA3mr2ywH4ZNTddp1GXKGIjsnQ0yFRerJT98E" },
+      headers: { "X-Mashape-Key": "OxcC40wXNtmshBb2QuFhuTG8xcUXp1huVw2jsnLmhpBuxYNOI8" },
       params: { instructionsRequired: false, offset: 0, query: req.body.data }
     }
     axios.get('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search', options)
       .then(resp => {
-        console.log(resp.data[0])
-        console.log(resp.data[0].imageUrls)
-        res.json(resp.data);
+        res.json(resp.data.results);
       })
       .catch(err => {
         console.log('Error: ', err);
@@ -20,13 +17,14 @@ const recipes = {
   getRecipe: (req, res, next) => {
     const options = {
       headers: {
-        "X-Mashape-Key": "f9B4tJmyAkmshyA3mr2ywH4ZNTddp1GXKGIjsnQ0yFRerJT98E"
+        "X-Mashape-Key": "OxcC40wXNtmshBb2QuFhuTG8xcUXp1huVw2jsnLmhpBuxYNOI8"
       }
     }
-
-    axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${req.body.id}/analyzedInstructions`, options)
+    axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${req.body.data}/analyzedInstructions`, options)
       .then( resp => {
-        res.json(resp.data);
+        console.log('resp.data', resp.data)
+        console.log('resp.DATATATATA', resp.data[0].steps[0].ingredients);
+        res.json(resp.data[0]);
       })
   }
 }

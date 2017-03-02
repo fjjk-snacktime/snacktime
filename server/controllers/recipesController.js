e973c8d7c005ebf0e10e9ce3c5fa25a20d2ca5bb
xfredlix <fred.m.li.93@gmail.com>
2017-03-02 15:36:02 -0800

    added a helper function in recipe controller to compare nutritional value

--- server/controllers/recipesController.js
+++ server/controllers/recipesController.js
@@ -24,11 +24,53 @@ const recipes = {
     };
     axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${req.body.data}/information`, options)
       .then( resp => {
-        console.log('resp.data', resp.data)
+        console.log('reLsp.data', resp.data)
         console.log('resp.DATATATATA', resp.data.analyzedInstructions[0].steps);
         res.json(resp.data);
       })
-  }
+  },
+  compareRecipes: (req, res, next) => {
+    const idArray = req.body.data;
+    const results = [];
+    const options = {
+      headers: { "X-Mashape-Key": "OxcC40wXNtmshBb2QuFhuTG8xcUXp1huVw2jsnLmhpBuxYNOI8" },
+    }
+    for (let id of idArray) {
+      axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${id}/information`, options)
+        .then( resp => {
+          for (let ingredient of resp.data.extendedIngredients) {
+            // console.log(ingredient.id, ingredient.amount, ingredient.unit)
+            const options = {
+              headers: { "X-Mashape-Key": "OxcC40wXNtmshBb2QuFhuTG8xcUXp1huVw2jsnLmhpBuxYNOI8" },
+              params: { amount: ingredient.amount, unit: ingredient.unit}
+            }
+            axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/${ingredient.id}/information`, options)
+              .then( resp => {
+                let obj = {};
+                // console.log(resp.data.nutrition.nutrients)
+                // let nutrient = resp.data.nutrition.nutrients[0]
+                for (let nutrient of resp.data.nutrition.nutrients) {
+                  // console.log('hi')
+                  if (!obj[nutrient.title]){
+                    obj[nutrient.title] = [nutrient.amount, nutrient.unit];
+                  } else {
+                    obj[nutrient.title][0] += nutrient.amount;
+                  }
+                }
+                // console.log(obj)
+                results.push(obj);
+                if (results.length === 2) {
+                  res.send(results);
+                }
+              }).catch( err => {
+                // console.log(err)
+              })
+          }
+          // console.log(results);
+        }).catch( err => {
+          console.log(err);
+        })
+    }
 }
 
 module.exports = recipes;

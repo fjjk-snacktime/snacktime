4d5023c6373fa4a2f244404ddb7965ce1eaac157
jeffvilla <jeffvilla@jeffvillas-MacBook-Pro.local>
2017-03-02 19:56:39 -0800

    (Enhancement) Add Snacktime logo to App component, change color theme to match teal logo

--- components/ShareFacebook.ios.js
+++ components/ShareFacebook.ios.js
@@ -1,39 +1,48 @@
-const FBSDK = require('react-native-fbsdk');
-const {
-  ShareDialog,
-} = FBSDK;
+import React , { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import FBSDK, { LoginManager } from 'react-native-fbsdk';
+const { LoginButton, AccessToken, ShareDialog } = FBSDK;
 
-// ...
+export default class ShareFacebook extends Component {
+  constructor(props) {
+    super(props);
 
-// Build up a shareable link.
-const shareLinkContent = {
-  contentType: 'link',
-  contentUrl: "https://facebook.com",
-  contentDescription: 'Wow, check out this great site!',
-};
-
-// ...
-
-// Share the link using the share dialog.
-shareLinkWithShareDialog() {
-  var tmp = this;
-  ShareDialog.canShow(this.state.shareLinkContent).then(
-    function(canShow) {
-      if (canShow) {
-        return ShareDialog.show(tmp.state.shareLinkContent);
+    this.state = {
+      shareLinkContent: {
+        contentType: 'link',
+        contentUrl: "https://dimeadozenclothing.com",
+        contentDescription: 'Wow, check out this great site!'
       }
     }
-  ).then(
-    function(result) {
+  }
+
+  shareLinkWithShareDialog() {
+  ShareDialog.canShow(this.state.shareLinkContent)
+    .then( canShow => {
+      if (canShow) {
+        return ShareDialog.show(this.state.shareLinkContent);
+      }
+    }
+  )
+    .then( result => {
       if (result.isCancelled) {
         alert('Share cancelled');
       } else {
-        alert('Share success with postId: '
-          + result.postId);
+        console.log('result', Object.keys(result));
       }
-    },
-    function(error) {
+    }, error => {
       alert('Share fail with error: ' + error);
+    });
+  }
+
+  render() {
+      return (
+        <View>
+          <TouchableOpacity
+            onPress={this.shareLinkWithShareDialog.bind(this)}
+          >
+            <Text>Share on Facebook!</Text>
+          </TouchableOpacity>
+        </View>
+      );
     }
-  );
-}

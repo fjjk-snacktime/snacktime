f71de0dde4cb22b3aaaae5903c23f15dfed512f5
jeffvilla <jeffvilla@jeffvillas-MacBook-Pro.local>
2017-03-05 17:34:05 -0800

    (Feat) Add action and reducer for AddIngredient component

---
+++ actions/addIngredientActions.ios.js
@@ -0,0 +1,12 @@
+export default addIngredientActions = {
+  showSearch: () => {
+    return {
+      type: 'SHOW_SEARCH'
+    }
+  },
+  rendering: () => {
+    return {
+      type: 'RENDERING'
+    }
+  }
+}

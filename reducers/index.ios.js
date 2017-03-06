604cc58ef97c0a558e5ef7bc254ac6689596f005
jeffvilla <jeffvilla@jeffvillas-MacBook-Pro.local>
2017-03-05 17:29:47 -0800

    (Enhancement) Add AddIngredientReducer

--- reducers/index.ios.js
+++ reducers/index.ios.js
@@ -1,7 +1,9 @@
 import {combineReducers} from "redux";
 
 import app from './appReducers.ios.js';
+import addIngredient from './addIngredientReducers.ios.js';
 
 export default combineReducers({
   app,
+  addIngredient,
 })

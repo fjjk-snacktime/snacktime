fca32340fbc17ae1532f7a4cc103b9738b1def0c
jeffvilla <jeffvilla@jeffvillas-MacBook-Pro.local>
2017-03-05 17:35:15 -0800

    (Feat) Implement AddIngredientCamera & AddIngredientGoogleResults components

---
+++ components/AddIngredientGoogleResults.ios.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Text, Image, TouchableHighlight, View, ListView } from 'react-native';
+import FoodpairResults from './FoodpairResults.ios.js';
+import helpers from '../helpers/helpers.js';
+import styles from '../styles.ios.js';
+import AddIngredient from './AddIngredient.ios.js'
+
+export default class GoogleResults extends Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(props.results),
+      food: ''
+    }
+    this.onPress = this.onPress.bind(this);
+  };
+
+  changeNavigation() {
+    const currentIngredients = this.props.currentIngredients;
+    currentIngredients.push(this.state.food);
+    this.props.navigator.push({
+      component: AddIngredient,
+      passProps: {
+        currentIngredients: currentIngredients,
+        store: this.props.store
+      }
+    })
+  }
+
+  onPress(data) {
+    this.setState({ food: data }, this.changeNavigation);
+  }
+
+  goBack() {
+    this.props.navigator.pop();
+  }
+
+  render() {
+    return (
+      <View style={styles.resultsList}>
+        <View style={styles.navigationResults}>
+          <View style={styles.resultsTitle}> 
+            <TouchableHighlight style={styles.backButton} onPress={this.goBack.bind(this)}>
+              <Image style={styles.backButtonImage} source={{uri: 'https://cdn0.iconfinder.com/data/icons/vector-basic-tab-bar-icons/48/back_button-128.png'}} />
+            </TouchableHighlight>
+            <Text style={styles.resultsTitleText}> Ingredient to Add</Text>
+          </View>
+        </View>
+        <ListView
+          dataSource={this.state.dataSource}
+          style={styles.resultsList}
+          renderRow={(rowData, i) => (
+            <TouchableHighlight
+              onPress={this.onPress.bind(this, rowData)}
+              underlayColor="blue"
+              >
+              <View style={styles.googleListItem}>
+                <Image style={styles.arrow} source={{uri: 'https://maxcdn.icons8.com/Android_L/PNG/512/User_Interface/circled_chevron_right-512.png'}} />
+                <Text style={styles.text}>{rowData}</Text>
+              </View>
+            </TouchableHighlight>
+            )}
+        />
+      </View>
+    );
+  }
+}

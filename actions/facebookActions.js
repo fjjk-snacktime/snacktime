a9dc28be6517897641e2c901473221485137c80f
Kevin Wong <kkwokwai22@gmail.com>
2017-03-07 14:54:38 -0800

    add a facebookAction

--- actions/facebookActions.js
+++ actions/facebookActions.js
@@ -1,10 +1,19 @@
+import FBSDK, { LoginManager } from 'react-native-fbsdk';
+const { LoginButton, AccessToken, ShareDialog } = FBSDK;
+
 export default facebookActions = {
 
-  isAuthenticated: () => {
-    return {
-      type: 'IS_AUTHENTICATED'
+    facebookAction: () => {
+      return {
+        type: 'IS_AUTHENTICATED',
+        payload: AccessToken.getCurrentAccessToken()
+        .then((data) => {
+          console.log('facebook data change to became payload', data)
+          return data
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      }
     }
-  },
-
-
-} 
+}

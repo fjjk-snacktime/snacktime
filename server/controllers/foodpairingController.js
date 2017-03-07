3c8d332eaac6ca535b293cdfd0f9abadef2f2f83
Joseph Lee <joey@MacBook-Pro.local>
2017-03-06 20:03:17 -0800

    (config) Hid API keys

--- server/controllers/foodpairingController.js
+++ server/controllers/foodpairingController.js
@@ -1,8 +1,10 @@
+const pairingID = require('./apiKeys.js').pairingID;
+const pairingKey = require('./apiKeys.js').pairingKey;
 const axios = require('axios')
 
 const foodpairing = {
   getFoodpairing: (req, res, body) => {
-    const options = { headers: { 'X-Application-ID': 'b5378ca6', 'X-Application-Key': '690be2968f8f08b26fcc1f2c9c8f5b90' } };
+    const options = { headers: { 'X-Application-ID': pairingID, 'X-Application-Key': pairingKey } };
     axios.get(`https://api.foodpairing.com/ingredients?q=${req.body.data}`, options)
       .then( response => {
         axios.get(`https://api.foodpairing.com/ingredients/${response.data[0].id}/pairings`, options)
@@ -21,7 +23,7 @@ const foodpairing = {
     const ingredients = req.body.data;
     var ingredientIds = [];
 
-    const options = { headers: { 'X-Application-ID': 'b5378ca6', 'X-Application-Key': '690be2968f8f08b26fcc1f2c9c8f5b90' } };
+    const options = { headers: { 'X-Application-ID': pairingID, 'X-Application-Key': pairingKey } };
     for (let i = 0; i < ingredients.length; i++) {
       axios.get(`https://api.foodpairing.com/ingredients?q=${ingredients[i]}`, options)
         .then( response => {
@@ -44,7 +46,7 @@ const foodpairing = {
   },
   getMultipleFoodpairings: (req, res, body) => {
     const ids = req.body.data.join(', ')
-    const options = { headers: { 'X-Application-ID': 'b5378ca6', 'X-Application-Key': '690be2968f8f08b26fcc1f2c9c8f5b90' } };
+    const options = { headers: { 'X-Application-ID': pairingID, 'X-Application-Key': pairingKey } };
     axios.get(`https://api.foodpairing.com/ingredients/${ids}/pairings`, options)
       .then( response => {
         console.log('multiple foodpairings', response.data);

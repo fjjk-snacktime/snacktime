2e2688bf349ffb20fda587824370d3c5531f44e9
Kevin Wong <kkwokwai22@gmail.com>
2017-03-07 14:58:40 -0800

    added a reducer for logging

--- reducers/facebookReducer.js
+++ reducers/facebookReducer.js
@@ -1,13 +1,16 @@
-export default function addIngredientReducer(state={
-isAuthenticated: false}, action) {
+export default function facebookReducer (
+  state = {
+    isAuthenticated: false,
+    payload: null
+  }, action) {
   switch (action.type) {
     case 'IS_AUTHENTICATED': {
       return {
         ...state,
         isAuthenticated: true,
+        payload: action.payload
       }
     }
   }
-  
   return state;
 }

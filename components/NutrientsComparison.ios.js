9eba6778d0b76b891e9bf07d4415340a757f3e9f
xfredlix <fred.m.li.93@gmail.com>
2017-03-06 16:05:11 -0800

    updated styling sheet on the nutrients comparison and flex box styling

--- components/NutrientsComparison.ios.js
+++ components/NutrientsComparison.ios.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Image, TextInput, ListView, TouchableHighlight } from 'react-native';
+import { Text, View, Image, TextInput, ListView, TouchableHighlight, ScrollView } from 'react-native';
 import helpers from '../helpers/helpers.js';
 import Recipe from './Recipe.ios.js';
 import styles from '../styles.ios.js';
@@ -38,45 +38,52 @@ export default class NutrientsComparison extends Component {
     const compareNutrients = Object.keys(this.props.results[0]).map((nutrient, i) => {
       if (this.props.results[0][nutrient][0] > this.props.results[1][nutrient][0]) {
         return (
-          <Text style = {[styles.ingredientListText, {color: 'green'}]} key={i}>{nutrient}: {Math.floor(Math.abs(this.props.results[0][nutrient][0] - this.props.results[1][nutrient][0]))} {this.props.results[1][nutrient][1]}</Text>
+          <Text style = {[styles.ingredientListText, {color: '#458B00'}]} key={i}>⇦  {nutrient}: {Math.floor(Math.abs(this.props.results[0][nutrient][0] - this.props.results[1][nutrient][0]))} {this.props.results[1][nutrient][1]}</Text>
         )
       } else {
         return (
-          <Text style = {[styles.ingredientListText, {color: 'red'}]} key={i}>{nutrient}: {Math.floor(Math.abs(this.props.results[0][nutrient][0] - this.props.results[1][nutrient][0]))} {this.props.results[1][nutrient][1]}</Text>
+          <Text style = {[styles.ingredientListText, {color: '#B22222'}]} key={i}>{nutrient}: {Math.floor(Math.abs(this.props.results[0][nutrient][0] - this.props.results[1][nutrient][0]))} {this.props.results[1][nutrient][1]}  ⇨</Text>
         )
       }
     })
     return (
       <View style={styles.resultsList}>
-          <View style={styles.resultsTitle}> 
-            <TouchableHighlight style={styles.backButton} onPress={this.goBack.bind(this)}>
-              <Image style={styles.backButtonImage} source={{uri: 'https://cdn0.iconfinder.com/data/icons/vector-basic-tab-bar-icons/48/back_button-128.png'}} />
-            </TouchableHighlight>
-            <Text style={styles.resultsTitleText}>Nutritional Comparison:</Text>
-          </View>
+        <View style={styles.resultsTitle}> 
+          <TouchableHighlight style={styles.backButton} onPress={this.goBack.bind(this)}>
+            <Image style={styles.backButtonImage} source={{uri: 'https://cdn0.iconfinder.com/data/icons/vector-basic-tab-bar-icons/48/back_button-128.png'}} />
+          </TouchableHighlight>
+          <Text style={styles.compareRecipeText}>Nutritional Comparison:</Text>
+        </View>
         <View style={styles.compareNutrientsContainer}>
-          <TouchableHighlight style={styles.compareRecipe} onPress={this.selectRecipe.bind(this, this.props.ids[0], this.props.titles[0], this.props.images[0])} >
-            <View style={styles.compareRecipe} >
-              <View style={styles.compareHeader}>
+          <View style={styles.compareNutrientsHeader} >
+            <TouchableHighlight style={styles.compareHeader} onPress={this.selectRecipe.bind(this, this.props.ids[0], this.props.titles[0], this.props.images[0])} >
+              <View style={[styles.compareHeader, {backgroundColor: '#3B5323'}]}>
                 <Text style={styles.compareRecipeTitle} >{this.props.titles[0]} </Text>
                 <Image style={styles.resultsPicture} source={{uri: this.props.images[0]}} />
               </View>
-              <View style={styles.compareRecipe}>{renderNutrients(0)}</View>
-            </View>
-          </TouchableHighlight>
-          <View style={styles.compareRecipe} >
-            <Text style={styles.compareRecipeTitle} > Comparison: </Text>
-            <View style={styles.compareRecipe}>{compareNutrients}</View>
-          </View>
-          <TouchableHighlight style={styles.compareRecipe} onPress={this.selectRecipe.bind(this, this.props.ids[0], this.props.titles[0], this.props.images[0])}>
-            <View style={styles.compareRecipe} >
-              <View style={styles.compareHeader}>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.compareHeader} onPress={this.selectRecipe.bind(this, this.props.ids[1], this.props.titles[1], this.props.images[1])} >
+              <View style={[styles.compareHeader, {backgroundColor: '#E9967A'}]}>
                 <Text style={styles.compareRecipeTitle} >{this.props.titles[1]} </Text>
                 <Image style={styles.resultsPicture} source={{uri: this.props.images[1]}} />
               </View>
-              <View style={styles.compareRecipe}>{renderNutrients(1)}</View>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.compareNutrientsHeader} >
+            <TouchableHighlight style={styles.compareRecipe} onPress={this.selectRecipe.bind(this, this.props.ids[0], this.props.titles[0], this.props.images[0])} >
+              <View style={[styles.compareRecipe, {backgroundColor: '#3B5323'}]} >
+                <ScrollView style={styles.compareRecipe}>{renderNutrients(0)}</ScrollView>
+              </View>
+            </TouchableHighlight>
+            <View style={styles.compareRecipe} >
+              <View style={styles.compareNutrients}>{compareNutrients}</View>
             </View>
-          </TouchableHighlight>
+            <TouchableHighlight style={styles.compareRecipe} onPress={this.selectRecipe.bind(this, this.props.ids[1], this.props.titles[1], this.props.images[1])}>
+              <View style={[styles.compareRecipe, {backgroundColor: '#E9967A'}]} >
+                <ScrollView style={styles.compareRecipe}>{renderNutrients(1)}</ScrollView>
+              </View>
+            </TouchableHighlight>
+          </View>
         </View>
       </View>
     )

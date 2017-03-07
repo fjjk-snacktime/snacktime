ee1fd50a28857f2c57747bc562700bdb4c68cab7
Kevin Wong <kkwokwai22@gmail.com>
2017-03-07 14:55:03 -0800

    pass down store to component user

--- components/App.ios.js
+++ components/App.ios.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  Alert,
   Text,
   View,
   Image,
@@ -14,6 +15,7 @@ import { connect } from "react-redux";
 import {bindActionCreators} from 'redux';
 import * as app from '../actions/appActions.ios.js';
 import getApp from '../reducers/appReducers.ios.js';
+import UserPage from './UserPage.ios.js';
 import camera from './Camera.ios.js';
 import FacebookLogin from './FacebookLogin.ios.js';
 
@@ -28,6 +30,14 @@ class App extends Component {
       borderWidth: 5,
     }
   }
+
+  changeToUserPage() {
+    this.props.navigator.push({
+      component: UserPage,
+      passProps: { store: this.props.store}
+    })
+  }
+
 
   changeNavigationHome() {
     this.props.navigator.push({
@@ -50,50 +60,87 @@ class App extends Component {
     const {state, actions} = this.props;
     if (state.rendering) {
       return (
-        <Image 
+        <Image
           source= {{uri: 'https://media.blueapron.com/assets/loader/pot-loader-6047abec2ec57c18d848f623c036f7fe80236dce689bb48279036c4f914d0c9e.gif'}}
           style = {styles.loadingGif}
         />
       )
     }
-    return (
-      <View style={[styles.container]}>
-        <View style={[styles.navigation]} >
-          <Nav
-            changeNavigationCamera={this.changeNavigationCamera.bind(this)}
-          />
+    if (state.showSearchBar) {
+      return (
+        <View style={[styles.container]}>
+          <View style={[styles.navigation]} >
+            <Nav
+              changeNavigationCamera={this.changeNavigationCamera.bind(this)}
+              changeToUserPage={this.changeToUserPage.bind(this)}
+            />
+          </View>
+          <View style={styles.app} >
+            <Image style={styles.welcomeImage} source={require('../snacktimewelcome.jpg')}/>
+            <FacebookLogin />
+          </View>
+          <View style={[styles.searchBarPictureFrame]} >
+            <SearchBar store={this.props.store} navigator={this.props.navigator} rendering={actions.rendering} lagOut={actions.laggedOut} isRendering={state.rendering}/>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Switch
+              onValueChange={actions.showSearchBar}
+              style={styles.switch}
+              value={state.showSearchBar} />
+          </View>
         </View>
-        <View style={styles.app} >
-          <Image style={styles.welcomeImage} source={require('../public/snacktimewelcome.jpg')}/>
-          <FacebookLogin />
+      );
+    } else {
+      return (
+        <View style={[styles.container]}>
+          <View style={[styles.navigation]} >
+            <Nav
+              changeNavigationCamera={this.changeNavigationCamera.bind(this)}
+              changeToUserPage={this.changeToUserPage.bind(this)}
+            />
+          </View>
+          <View style={styles.app} >
+            <Image style={styles.welcomeImage} source={require('../snacktimewelcome.jpg')}/>
+            <FacebookLogin />
+          </View>
+          <TouchableHighlight style={[styles.buttonView]} onPress={this.changeNavigationCamera.bind(this)}>
+            <Image style={[styles.takePicture]} source={{uri: 'https://s3.amazonaws.com/features.ifttt.com/newsletter_images/2015_February/camera512x512+(1).png'}}/>
+          </TouchableHighlight>
+          <View style={{flexDirection: 'row'}}>
+            <Switch
+              onValueChange={actions.showSearchBar}
+              style={styles.switch}
+              value={state.showSearchBar}
+              />
+          </View>
         </View>
         { /* conditional camera / search bar render */
           state.showSearchBar
-            ? 
+            ?
               (
                 <View style={[styles.searchBarPictureFrame]} >
                   <SearchBar store={this.props.store}
                              navigator={this.props.navigator}
                              rendering={actions.rendering}
-                             lagOut={actions.laggedOut} 
+                             lagOut={actions.laggedOut}
                              isRendering={state.rendering}
                   />
                 </View>
               )
-            : 
+            :
               (
-                <TouchableHighlight style={[styles.buttonView]} 
+                <TouchableHighlight style={[styles.buttonView]}
                                     onPress={this.changeNavigationCamera.bind(this)}
                 >
-                  <Image 
+                  <Image
                     style={[styles.takePicture]}
                     source={{uri: 'https://s3.amazonaws.com/features.ifttt.com/newsletter_images/2015_February/camera512x512+(1).png'}}
                   />
                 </TouchableHighlight>
               )
         }
-        <View style={{flexDirection: 'row'}}> 
-          <Switch 
+        <View style={{flexDirection: 'row'}}>
+          <Switch
             onValueChange={actions.showSearchBar}
             style={styles.switch}
             value={state.showSearchBar}

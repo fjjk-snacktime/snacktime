9b44f2b532718a2b517389f97b44d421877753c2
Kevin Wong <kkwokwai22@gmail.com>
2017-03-08 11:41:24 -0800

    need to add facebook reducer

--- reducers/index.ios.js
+++ reducers/index.ios.js
@@ -2,10 +2,15 @@ import {combineReducers} from "redux";
 
 import app from './appReducers.ios.js';
 import addIngredient from './addIngredientReducers.ios.js';
-import facebook from './facebookReducer.js';
+import facebook from './facebookReducer.ios.js';
 
 export default combineReducers({
   app,
   addIngredient,
   facebook
 })
+
+
+// import AddRecipe from './AddRecipe.ios.js';
+
+//<AddRecipe info={this.props.recipe} ingredients={this.state.ingredients}/>

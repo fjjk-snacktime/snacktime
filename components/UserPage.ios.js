56bff8079f0aa68394c0382d47b7d25c180e1064
Kevin Wong <kkwokwai22@gmail.com>
2017-03-08 18:51:37 -0800

    a user page

--- components/UserPage.ios.js
+++ components/UserPage.ios.js
@@ -6,6 +6,7 @@ import {
         Image,
         Navigator,
         Button,
+        ScrollView,
         ListView,
         Icon,
         TouchableHighlight,
@@ -19,7 +20,8 @@ class UserPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dataSource: []
+      dataSource: [],
+      ingredients: []
     };
   }
 
@@ -28,7 +30,11 @@ class UserPage extends Component {
   }
 
   componentWillMount() {
-    this.gettt();
+    if(!this.props.state.payload) {
+      console.log('nothing')
+    } else {
+      this.getData();
+    }
   }
 
   // create NewUser if not find in DB
@@ -46,7 +52,17 @@ class UserPage extends Component {
     });
   }
 
-  gettt() {
+  // renderIngredients() {
+  //   let ingredients = [];
+  //   for (let ingredient of this.props.recipe.extendedIngredients) {
+  //     ingredients.push(ingredient.originalString);
+  //   }
+  //   this.setState({
+  //     ingredients: ingredients
+  //   })
+  // }
+
+  getData() {
     var local = 'http://localhost:8000/'
     var FacebookUserID = this.props.state.payload.userID
     axios.get(`${local}database`, {
@@ -60,25 +76,48 @@ class UserPage extends Component {
         this.setState({
           dataSource: userrecipedata
         })
+      })
+      .catch((error) => {
+      this.createNewUserIfNotAccount(FacebookUserID)
     })
-    .catch((error) => {
-      console.log('no data')
-    });
   }
 
   render() {
-    console.log(this.state.dataSource)
+    if (this.props.state.isAuthenticated) {
     return (
       <View>
-        <Text style={{marginTop: 100}}>UserHomePage</Text>
+        <Text style={{marginTop: 25}}>UserHomePage</Text>
         <TouchableHighlight style={styles.backButtonCamera} onPress={this.goBack.bind(this)}>
           <Image style={styles.backButtonImage} source={{uri: 'https://cdn0.iconfinder.com/data/icons/vector-basic-tab-bar-icons/48/back_button-128.png'}} />
         </TouchableHighlight>
+        <Text>Your FavoriteRecipe</Text>
+        <ScrollView style={{marginBottom: 110}}>
         {this.state.dataSource.map((data, index) => (
-          <Text key={index} style={{marginTop: 100}}>{data.name}</Text>
+          <View key={index} style={styles.ingredientContainer}>
+            <View style={styles.ingredientList}>
+              <Text>Recipe Name: {data.name}</Text>
+                <View style={styles.recipeImageAndIcons}>
+                  <Image source={{uri: data.image}} style={styles.recipeImage} />
+                </View>
+                <TouchableHighlight style={styles.backButtonCamera} onPress={this.goBack.bind(this)}>
+                  <Image style={styles.backButtonImage} source={{uri: 'https://cdn0.iconfinder.com/data/icons/vector-basic-tab-bar-icons/48/back_button-128.png'}} />
+                </TouchableHighlight>
+            </View>
+          </View>
         ))}
+        </ScrollView>
       </View>
     )
+  } else {
+    return (
+      <View style={{marginTop: 75}}>
+        <Text>Not login</Text>
+        <TouchableHighlight style={styles.backButtonCamera} onPress={this.goBack.bind(this)}>
+          <Image style={styles.backButtonImage} source={{uri: 'https://cdn0.iconfinder.com/data/icons/vector-basic-tab-bar-icons/48/back_button-128.png'}} />
+        </TouchableHighlight>
+      </View>
+      )
+    }
   }
 }
 
@@ -86,3 +125,6 @@ export default connect(state => ({
     state: state.facebook
   }), null
 )(UserPage);
+
+// <Text style={styles.recipeTitle2}>Ingredients:</Text>
+// <Text style={styles.ingredientListText2}>{ingredients}</Text>
